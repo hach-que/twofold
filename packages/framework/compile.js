3080b67c9316dd7e7d25c95aaf8f10f4c7dc7e7f
June Rhodes <jrhodes@redpoint.games>
2026-04-06 13:28:07 +1000

    Build and publish packages to GitHub

--- packages/framework/compile.js
+++ packages/framework/compile.js
@@ -1,5 +1,45 @@
 import { build } from "esbuild";
+import { readdirSync, readFileSync, writeFileSync } from "fs";
 import { rm } from "fs/promises";
+import path from "path";
+
+const rewriteImports = {
+  name: "rewrite-imports",
+  async setup(build) {
+    let outDir;
+    build.onStart(() => {
+      const options = build.initialOptions;
+      const absWorkingDir = options.absWorkingDir ?? process.cwd();
+      outDir = options.outdir
+        ? path.resolve(absWorkingDir, options.outdir)
+        : options.outfile
+          ? path.dirname(path.resolve(absWorkingDir, options.outfile))
+          : null;
+    });
+    build.onEnd(async (args) => {
+      if (outDir) {
+        for (const file of readdirSync(outDir, {
+          recursive: true,
+          withFileTypes: true,
+        })) {
+          if (file.isFile() && file.name.endsWith(".js")) {
+            const originalContents = readFileSync(
+              path.join(file.parentPath, file.name),
+              "utf-8",
+            );
+            const newContents = originalContents.replaceAll(
+              /"@twofold\/([a-z-]+)"/g,
+              `"@${process.env.PACKAGE_NAMESPACE}/$1"`,
+            );
+            if (originalContents != newContents) {
+              writeFileSync(path.join(file.parentPath, file.name), newContents);
+            }
+          }
+        }
+      }
+    });
+  },
+};
 
 async function main() {
   let dir = new URL("./dist/", import.meta.url);
@@ -13,6 +53,10 @@ async function main() {
     outdir: "./dist/backend",
     packages: "external",
     platform: "node",
+    plugins:
+      process.env.PACKAGE_NAMESPACE !== undefined
+        ? [rewriteImports]
+        : undefined,
   });
 }
 
